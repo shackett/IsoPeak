ff7cd725989f5d9df24020cd8c079d0bd2a1c924
shackett <shackett@princeton.edu>
2011-07-09 13:22:46 -0400

    7.9.11

--- AddNovAnalysis.R
+++ AddNovAnalysis.R
@@ -103,7 +103,7 @@ nstd <- length(combinedProbs[,1])
 
 #compare against all standards - combinedProbs$mass
 
-MZe <- log(dnorm(sapply(combinedProbs$mass, masserror, standard = pMZ) + MZoffsetrack[nanneal], mean = 0, sd = 4), base = 2)
+MZe <- log(dnorm(sapply(combinedProbs$mass, masserror, standard = pMZ) + MZoffsetrack[nanneal], mean = 0, sd = 1), base = 2)
 RTe <- log(dnorm(sapply(combinedProbs$RT, RTdiff, standard = pRT), mean = 0, sd = pRT*RT.SDtrack[nanneal]), base = 2)
 SIZe <- t(log(probMat %*% t(peaksizeMat), base = 2))
 posL <- MZe + RTe + SIZe
@@ -127,12 +127,9 @@ MZlikE <- sum(apply(LIKmat, 1, max))/npeaks
 
 com <- 169
 
-GauS.w.Adduct <- function(x)
-
-
 ############# Determine likelihood of a set of peaks corresponding to a compouds isotopes given the current evidence from peak size and position (posL) and the observed ratios of isotopes ###############
 
-GaussianLik <- function(com, coToiso, posL, peaksizeMat, probMat, npeaks, h, SDlmMat, HETbase, pMZ, pRT, combinedAdds){
+GauS.w.Adduct <- function(com, coToiso, posL, peaksizeMat, probMat, npeaks, h, SDlmMat, HETbase, pMZ, pRT, combinedAdds, ADDUCT.OUT, RT.UNKNOWN){
 #single peaks have the right ratio
 #print(com)
 subprob <- probMat[coToiso[,com],]
@@ -181,14 +178,16 @@ RTgrid[is.na(RTgrid)] <- 0
 
 permRTs <- apply(RTgrid*matrix(RT.weights, ncol = length(validP[1,]), nrow = length(validP[,1]), byrow = TRUE), 1, sum, na.rm = TRUE)/ifelse(is.na(validP), 0, 1)%*%RT.weights
 
-logL.RT <- log(dGuas(x=RTgrid, mu = matrix(permRTs, ncol = length(validP[1,]), nrow = length(validP[,1])), sd = matrix(0.1, ncol = length(validP[1,]), nrow = length(validP[,1]))), base = 2) * ifelse(is.na(validP), 0, 1)
+logL.RT <- log(gausD(x=RTgrid, mu = matrix(permRTs, ncol = length(validP[1,]), nrow = length(validP[,1])), sd = matrix(0.1, ncol = length(validP[1,]), nrow = length(validP[,1]))), base = 2) * ifelse(is.na(validP), 0, 1)
 logL.RT[is.nan(logL.RT)] <- NA
 logL.RT.perm <- apply(logL.RT, 1, sum, na.rm = TRUE)
 
 ######
 
 validRT <- validP[logL.RT.perm > 0,]
+RT.perm <- permRTs[logL.RT.perm > 0]
 
+if(length(validRT[,1]) != 0){
 
 
 
@@ -288,7 +287,7 @@ tMatFULL[k, combosFULL[,1]] <- 1}
 sharedPeakPairs <- rbind(sharedPeakPairs, tMat)	
 sharedPnumVec <- c(sharedPnumVec, rep(sharedPeaks[i], times = length(tMat[,1])))
 sharedPeakPairsFULL <- rbind(sharedPeakPairsFULL, tMatFULL)
-}}
+}
 
 rTemp <- validRT
 rTemp[is.na(rTemp)] <- 0
@@ -303,7 +302,7 @@ LIKadjust <- log(exp((Tcombos %*% sharedPeakPairsFULL)*EVAL[,colnames(EVAL) == k
 
 EVAL[,colnames(EVAL) == k] <- EVAL[,colnames(EVAL) == k] + ifelse(abs(LIKadjust) == Inf, 1, LIKadjust)
 
-	}
+	}}
 
 #######################################
 
@@ -313,6 +312,10 @@ EVALsum <- apply(EVAL, 1, sum)
 peakeval <- matrix(EVAL[order(EVALsum, decreasing = TRUE)[1:min(10, nperms)],], nrow = nsamples*npeakISO, byrow = TRUE)
 rownames(peakeval) <- rep(c(1:npeakISO), each = length(indies))
 
+RT.perm.eval <- RT.perm[order(EVALsum, decreasing = TRUE)[1:min(10, nperms)]]
+MU.perm.eval <- MUcolz[order(EVALsum, decreasing = TRUE)[1:min(10, nperms)],]
+
+
 outz <- matrix(sapply(c(1:npeakISO), factcond, peakeval), ncol = npeakISO)
 outz <- rbind(outz, rep(0, times = length(outz[1,])))
 
@@ -321,7 +324,9 @@ output <- data.frame(colZ, standard = c(1:length(coToiso[,1]))[coToiso[,com]][co
 output <- output[output$value != 0,]
 
 if(length(output[,1]) != 0){
-output}
+	
+if(ADDUCT.OUT <- FALSE){output}else{
+
 
 ############ Look for adducts of each peak in stacker
 
@@ -336,23 +341,151 @@ addMZmat <- (matrix(STD$mass, ncol = length(STD$mass), nrow = length(transM[,1])
 
 adductL <- length(unlist(t(addMZmat)))
 
+#points adduct type to corresponding columns of adduct isotopic variants * parent peak
 
+adduct.name <- unique(transM[,1])
+adduct.to.pbya <- matrix(data = NA, ncol = length(adduct.name), nrow = adductL)
+
+for(i in 1:length(adduct.name)){
+
+adduct.to.pbya[,i] <- rep(transM[,1] == adduct.name[i], times = length(STD[,1]))
+
+}
+
+#
 addMZmat <- matrix(unlist(t(matrix(addMZmat, ncol = length(STD$mass), nrow = length(transM[,1]), byrow = TRUE))), ncol = 4, nrow = adductL)
 
 addPmat <- matrix(sampleclass, ncol = nsamples, nrow = adductL, byrow = TRUE)
 
-addPmat <- ifelse(addPmat == "N", 1, 0)*matrix(unlist(t(matrix(STD$nLabp, ncol = length(STD$mass), nrow = length(transM[,1]), byrow = TRUE)*matrix(transM$nLab, ncol = length(STD$mass), nrow = length(transM[,1])))), ncol = 4, nrow = adductL) + ifelse(addPmat == "N", 0, 1)*matrix(unlist(t(matrix(STD$uLabp, ncol = length(STD$mass), nrow = length(transM[,1]), byrow = TRUE)*matrix(transM$unLab, ncol = length(STD$mass), nrow = length(transM[,1])))), ncol = 4, nrow = adductL) 
+addPmat <- ifelse(addPmat == "N", 1, 0)*matrix(unlist((matrix(STD$nLabp, ncol = length(STD$mass), nrow = length(transM[,1]), byrow = TRUE)*matrix(transM$nLab, ncol = length(STD$mass), nrow = length(transM[,1])))), ncol = 4, nrow = adductL) + ifelse(addPmat == "N", 0, 1)*matrix(unlist((matrix(STD$uLabp, ncol = length(STD$mass), nrow = length(transM[,1]), byrow = TRUE)*matrix(transM$unLab, ncol = length(STD$mass), nrow = length(transM[,1])))), ncol = 4, nrow = adductL) 
 
 #matrix(unlist((matrix(STD$nLabp, ncol = length(STD$mass), nrow = length(transM[,1]), byrow = TRUE) + matrix(transM$nLab, ncol = length(STD$mass), nrow = length(transM[,1]))))
 
-
-matrix(paste(t(matrix(STD$mass, ncol = length(STD$mass), nrow = length(transM[,1]), byrow = TRUE)), t(matrix(transM$adduct, ncol = length(STD$mass), nrow = length(transM[,1])))), ncol = 4, nrow = adductL)
-
+#matrix(paste(t(matrix(STD$mass, ncol = length(STD$mass), nrow = length(transM[,1]), byrow = TRUE)), t(matrix(transM$adduct, ncol = length(STD$mass), nrow = length(transM[,1])))), ncol = 4, nrow = adductL)
 
 
 
 
-#for expected i isotopes w/ known MZ and l adducts, generate a m*l matrix of excpected M/Z including the global offset 
+#PeakAddMZmat <- matrix(data = rep(pMZ, each = nsamples), ncol = nsamples*length(pMZ), nrow = length(addPmat[,1]), byrow = TRUE)
+
+#STDaddMZmat <- matrix(data = unlist(t(matrix(addMZmat, ncol = length(STD$mass), nrow = length(transM[,1]), byrow = TRUE))), ncol = nsamples*length(pMZ), nrow = length(transM[,1]))
+
+
+
+addMZeval <- t(log(dnorm(sapply(addMZmat[,1], masserror, standard = pMZ) + MZoffsetrack[nanneal], mean = 0, sd = 1), base = 2))
+
+addRTeval <- log(dnorm(sapply(RT.perm.eval, RTdiff, standard = pRT), mean = 0, sd = 0.1), base = 2)
+
+addSIZEeval <- log(addPmat %*% t(peaksizeMat), base = 2)
+
+for(k in 1:length(MU.perm.eval[,1])){
+	
+addLik <- addMZeval + addSIZEeval + matrix(addRTeval[,k], ncol = length(peaksizeMat[,1]), nrow = length(addPmat[,1]), byrow = TRUE)
+
+for(add in 1:length(adduct.name[,1])){
+	
+ind.addL <- addLik[adduct.to.pbya[,add],]
+add.nfacs <- apply(ind.addL, 1, sumthresh, thresh = -20, nvec = npeaks)
+	
+if(length(unlist(add.nfacs)) != 0){
+	
+stacker <- NULL
+for(i in 1:length(add.nfacs)){
+	if(length(unlist(add.nfacs[i]) > 0)){stacker <- rbind(stacker, data.frame(peaks = unlist(add.nfacs[i]), iso = i), data.frame(peaks = NA, iso = i))}else{stacker <- rbind(stacker, data.frame(peaks = NA, iso = i))	}}
+
+factlevels = unstack(stacker)
+levgrid <- expand.grid(factlevels)
+definedP <- ifelse(is.na(levgrid), 0, 1)
+
+validP <- levgrid[definedP%*%apply(addPmat[adduct.to.pbya[,add],], 1, max) > isoCov,]
+
+if(length(validP[,1]) != 0){
+
+colZ <- stacker[!is.na(stacker)[,1],]
+
+add.probMatsub <- addPmat[adduct.to.pbya[,add],]
+nuMiso <- length(levgrid[1,])
+npeakISO <- length(colZ[,1]) 
+nperms <- length(validP[,1])
+	
+	
+	
+	
+PMAT <- matrix(data = unlist(t(peaksizeMat[colZ[,1],])), ncol = nsamples*npeakISO, nrow = nperms, byrow = TRUE)
+#colnames(PMAT) <- rep(colZ[,1], each = nsamples)
+colnames(PMAT) <- rep(indies, times = npeakISO)
+
+PPROB <- matrix(data = unlist(t(as.data.frame(probMatsub[colZ[,2],]))), ncol = nsamples*npeakISO, nrow = nperms, byrow = TRUE)
+colnames(PPROB) <- rep(indies, times = npeakISO)
+
+sdPMAT <- matrix(data = peakSD(unlist(t(peaksizeMat[colZ[,1],])), h = 1, SDlmMat, HETbase), ncol = nsamples*npeakISO, nrow = nperms, byrow = TRUE)
+colnames(sdPMAT) <- rep(indies, times = npeakISO)
+
+aMAT <- matrix(data = unlist(peaksizeMat[colZ[,1],])/peakSD(unlist(peaksizeMat[colZ[,1],]), 1, SDlmMat, HETbase), ncol = nsamples*npeakISO, nrow = nperms, byrow = TRUE)
+colnames(sdPMAT) <- rep(indies, times = npeakISO)
+
+posLinfo <- NULL
+for(i in 1:length(colZ[,1])){
+	posLinfo <- c(posLinfo, posLsub[colZ[i,1], colZ[i,2]])
+}
+
+POSLMAT <- matrix(rep(posLinfo, each = nsamples), ncol = nsamples*npeakISO, nrow = nperms, byrow = TRUE) 
+
+
+#convert validRT to peak-sample x nperm format using colZ
+
+isoRep <- table(colZ[,2])*nsamples
+gridEXP <- validRT[,as.numeric(rep(names(isoRep), times = isoRep))]
+gridCOM <- matrix(data = (-1*rep(colZ[,1], each = nsamples)), ncol = nsamples*npeakISO, nrow = nperms, byrow = TRUE)
+
+USED <- ifelse(gridEXP+gridCOM == 0 & !is.na(gridEXP*gridCOM), 1, 0)
+
+# Take the sum of P for each samples x perm
+
+Psum <- USED*PPROB
+PAsum <- USED*PPROB*aMAT
+colnames(Psum) <- rep(indies, times = npeakISO)
+colnames(PAsum) <- rep(indies, times = npeakISO)
+
+MUcolz <- NULL
+if(nperms == 1){
+
+for(i in 1:nsamples){ 
+DEN <- Psum[,colnames(Psum) == i]*PAsum[,colnames(PAsum) == i]
+NUM <- PMAT[,colnames(PMAT) == i]*USED[,colnames(PMAT) == i]*aMAT[,colnames(PMAT) == i]*PPROB[,colnames(PMAT) == i]
+MUcolz <- cbind(MUcolz, NUM/DEN)
+}
+MUcolz <- ifelse(is.nan(MUcolz), 0, MUcolz)
+for(i in 1:nuniqueSamp){	
+MUcolz[,c(1:nsamples)[replicates == i]] <- mean(MUcolz[,c(1:nsamples)[replicates == i]])
+}}else{
+
+for(i in 1:nsamples){ 
+DEN <- apply(Psum[,colnames(Psum) == i], 1, sum)*apply(PAsum[,colnames(PAsum) == i], 1, sum)
+NUM <- apply(PMAT[,colnames(PMAT) == i]*USED[,colnames(PMAT) == i]*aMAT[,colnames(PMAT) == i]*PPROB[,colnames(PMAT) == i], 1, sum)
+MUcolz <- cbind(MUcolz, NUM/DEN)
+}
+MUcolz <- ifelse(is.nan(MUcolz), 0, MUcolz)
+for(i in 1:nuniqueSamp){	
+MUcolz[,c(1:nsamples)[replicates == i]] <- apply(MUcolz[,c(1:nsamples)[replicates == i]], 1, mean)
+}}
+
+
+
+
+
+	}}}
+
+
+#######
+
+
+
+
+}}}}}}
+	
+#for expected i isotopes w/ known MZ and l adducts, generate a m*l matrix of expected M/Z including the global offset 
+		
 MZtransform <- function(combinedAdds, STD){
 STDmz <- STD$mass
 abundSTDmz <- STD[(STD$uLabp + STD$nLabp) > 0.1,]
@@ -365,13 +498,6 @@ for(i in 1:length(combinedAdds[,1])){
 		transM <- rbind(transM, data.frame(adduct = combinedAdds$adduct[i], add = combinedAdds$weightch[i] + combinedAdds$nmol[i]*abundSTDmz$mass, scale = rep(abs(1/combinedAdds$charge[i])), unLab = combinedAdds$uLabp[i]*abundSTDmz$uLabp^(combinedAdds$nmol[i]), nLab = combinedAdds$nLabp[i]*abundSTDmz$nLabp^(combinedAdds$nmol[i])))
 		}}
 transM}
-
-
-
-
-}}}
-		
-
 
 
 
